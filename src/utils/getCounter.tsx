ec0e8db78a75092428b3f89d89320ecb17297957
Danil Dovgal <91081974+qweblessed@users.noreply.github.com>
2022-10-30 19:11:38 +0200

    fix:formatted

--- src/utils/getCounter.tsx
+++ src/utils/getCounter.tsx
@@ -5,7 +5,7 @@ import { Counter__factory } from "../typechain";
 
 export const getCounter = async () => {
   const provider = new ethers.providers.JsonRpcProvider(
-    `https://eth-${process.env.REACT_APP_NETWORK_NAME}.g.alchemy.com/v2/${process.env.REACT_APP_ALCHEMY_API_KEY}`    
+    `https://eth-${process.env.REACT_APP_NETWORK_NAME}.g.alchemy.com/v2/${process.env.REACT_APP_ALCHEMY_API_KEY}`
   );
 
   const counterContract = Counter__factory.connect(COUNTER_ADDRESS, provider);
@@ -14,5 +14,3 @@ export const getCounter = async () => {
 
   return counter;
 };
-
-
